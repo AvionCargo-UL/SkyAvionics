29b2c64525416ed66c1f2af845a8f2da4daedc38
Babouche <clnabergel@hotmail.fr>
2024-10-06 18:26:55 -0400

    Aruco drawer (#4)

--- src/domain/vision/aruco/aruco_factory.py
+++ src/domain/vision/aruco/aruco_factory.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 
 from src.domain.common.position import Position
@@ -13,6 +15,10 @@ class ArucoFactory:
         position_y = float(np.mean(corners[0][:, 0]))
         return Position(position_x, position_y)
 
+    def __create_corners(self, corners: np.ndarray) -> List[Position]:
+        return [Position(x, y) for x, y in corners[0]]
+
     def create(self, identifier: int, corners: np.ndarray) -> Aruco:
         position = self.__create_position(corners)
-        return Aruco(identifier, position)
+        corners = self.__create_corners(corners)
+        return Aruco(identifier, position, corners)
