bcd026a44c1a43df9abc4cd0182710eea812c5d2
Babouche <clnabergel@hotmail.fr>
2024-10-08 16:57:10 -0400

    Move arucos drawer to controller (#6)

--- src/domain/vision/vision_controller.py
+++ src/domain/vision/vision_controller.py
@@ -2,14 +2,19 @@ from typing import List
 
 import cv2
 
+from src.domain.vision.aruco.aruco_drawer import ArucoDrawer
 from src.domain.vision.aruco.aruco_factory import Aruco
 from src.domain.vision.aruco.aruco_detector import ArucoDetector
 
 
 class VisionController:
     def __init__(self):
+        self.__aruco_drawer = ArucoDrawer()
         self.__aruco_detector = ArucoDetector()
 
     def do_aruco_detection(self, image: cv2.Mat) -> List[Aruco]:
         grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return self.__aruco_detector.detect(grayscale_image)
+
+    def draw_on_aruco(self, frame: cv2.Mat, arucos: List[Aruco]):
+        self.__aruco_drawer.draw(frame, arucos)
